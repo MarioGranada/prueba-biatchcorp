18b7ad5ca2132b95626468fe907b8ad1d2d581db
Mario Granada <mario.granada190@gmail.com>
2012-07-05 13:54:20 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PruebaBiatchcorp::Application.config.secret_token = '860d4478c2be33df1caecaac88c69efa62edf181de52ac69a3df5027ed8c083910b0ac0581d0fed2c1bd4f2c18ba8c922074e7d96bad53bcd0b8ece582f28030'
